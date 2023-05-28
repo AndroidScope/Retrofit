e97cf3023df16625b4fe71d4ad71df06e5d4493e
AndroidScope <130051402+AndroidScope@users.noreply.github.com>
2023-05-28 19:31:33 +0530

    Update MainActivity.java
    api

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -14,7 +14,7 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 private Retrofit mclient;
-private Api api;
+private Api ap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
